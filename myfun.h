71201c5872bad01ba65986dea628275e3f695bf8
徐永杰 <yongjiexu19901016@gmail.com>
2014-09-25 11:24:56 +0800

    Update myfun.h

--- myfun.h
+++ myfun.h
@@ -1,6 +1,19 @@
-#ifndef MYFUN.H
-#define MYFUN.H
+#ifndef MYFUN_H
+#define MYFUN_h
+#include<fstream>
+using namespace std;
 void SetScreenGrid();
 void SetSysCaption();
 void MainLoop();
+void ClearScreen();
+void ShowWelcome();
+void ShowRootMenu();
+int GetSelect();
+void WaitUser();
+void GuideInput();
+long GetFileLength(ifstream &ifs);
+void WaitView(int nextPage, int iPage);
+void ViewData(int iSelectPage = 1);
+void DeleteBookFromFile();
 #endif // MYFUN
+
